f8d865f90db32d03ce007c1745b9efcfaf9bcba1
lilithsatan666 <lilithsatan666@proton.me>
2026-04-01 11:30:10 -0700

    Create electronicMoneyInstitution.jl

---
+++ electronicMoneyInstitution.jl
@@ -0,0 +1,60 @@
+Dear Ira
+
+Welcome to Modulr Sandbox. Below you'll find your Account details and Sandbox API key. We have credited your first test account with GBP 50,000.00 so you can simulate payments.
+Remember, these account details and the funding will not work in the real world!
+
+You'll receive a separate email with a link to create a password to access the Modulr Sandbox user interface.
+
+Account details
+
+Customer ID:
+
+C213WDXW
+
+Account number:
+
+05774623
+
+Sort code:
+
+00-00-00
+
+Account alias:
+
+MoneyDesk Lllc
+
+Account ID:
+
+A2100GWGCY
+
+Balance:
+
+GBP 50,000.00
+
+Sandbox API key
+
+dTwSUsf8oSmnte0_FF3rw4SD9bHjm5tAuz1VNzOHHYcxNzc1MDY3NjM3MDg2
+
+Secret
+
+3SzV2Jpk4MW_oa6hQqwJvbVcnnJwg5MQ9afpp9DBxDk=
+
+The base URL for the Modulr API sandbox environment is:
+
+https://api-sandbox.modulrfinance.com/api-sandbox/
+
+To get details on how to set-up HMAC, please follow this link .
+
+If you wish to try our Sandbox without setting up HMAC, you can use this base URL instead:
+
+The base URL (no HMAC) for the Modulr API sandbox environment is:
+
+https://api-sandbox.modulrfinance.com/api-sandbox-token/
+
+Thank you
+
+The Modulr admin team
+
+Go to Developer Centre
+
+You have been sent this email because you have signed up for access to Modulr Sandbox.
